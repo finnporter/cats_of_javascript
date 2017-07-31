6fffed73d36a9c93ccac9b8481864462ca03546e
finnporter <iamfinnporter@gmail.com>
2017-07-31 18:30:41 +0100

    removes hardcoded html cats and adds them via js

--- public/app.js
+++ public/app.js
@@ -45,6 +45,9 @@ var app = function() {
   };
 
   addCat("Witches", "Toast", "https://static.tumblr.com/f5a863b930ea6f3e1df6b538f8954c48/irceql1/RbCn2eqsj/tumblr_static_tumblr-cat-pics.jpg");
+  addCat("Boba", "Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
+  addCat("Barnaby", "Tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
+  addCat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
 };
 
 window.addEventListener('load', app);
