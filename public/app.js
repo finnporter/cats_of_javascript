485964fd3d36cfdde47aa9a4c78b0ec8979f487e
finnporter <iamfinnporter@gmail.com>
2017-07-31 17:38:29 +0100

    adds cat manually/hardcoded

--- public/app.js
+++ public/app.js
@@ -3,8 +3,25 @@ var app = function() {
   var ul = document.createElement('ul');
   ul.classList.add('cat');
 
-  var li = document.createElement('li');
-  li.innerHTML = ''
+  var nameli = document.createElement('li');
+  nameli.innerText = 'Name: Witches';
+
+  var foodli = document.createElement('li');
+  foodli.innerText = 'Favourite Food: Toast';
+
+  var imgli = document.createElement('li');
+
+  var img = document.createElement('img');
+  img.src = "http://static.tumblr.com/f5a863b930ea6f3e1df6b538f8954c48/irceql1/RbCn2eqsj/tumblr_static_tumblr-cat-pics.jpg";
+  img.width = "500";
+
+  ul.appendChild(nameli);
+  ul.appendChild(foodli);
+  ul.appendChild(imgli);
+  imgli.appendChild(img);
+
+  var cats = document.querySelector('#cats');
+  cats.appendChild(ul);
 }
 
 window.addEventListener('load', app);
