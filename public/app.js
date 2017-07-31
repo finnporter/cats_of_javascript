92ef8d116c26862e0e9986d0fdaf62494ccdeefe
finnporter <iamfinnporter@gmail.com>
2017-07-31 17:18:36 +0100

    initial commit

---
+++ public/app.js
@@ -0,0 +1,10 @@
+var app = function() {
+
+  var ul = document.createElement('ul');
+  ul.classList.add('cat');
+
+  var li = document.createElement('li');
+  li.innerHTML = ''
+}
+
+window.addEventListener('load', app);
